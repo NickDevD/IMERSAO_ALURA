d6a03b94b4c9c0229e432383199641b283fbdc2d
Nicholas Monteiro <nick.developerdev@gmail.com>
2024-09-06 17:01:05 -0300

    Funções de pesquisa responsiva adicionados

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+      titulo: "Senhor dos Anéis",
+      descricao: "Em uma terra antiga e mágica, um hobbit herda um anel poderoso que ameaça o mundo. Acompanhe Frodo e seus companheiros em uma jornada épica para destruir o Um Anel nas profundezas do Monte do Destino, antes que o Lorde das Trevas Sauron reconquiste a Terra-Média.",
+      link: "https://pt.wikipedia.org/wiki/O_Senhor_dos_An%C3%A9is_(s%C3%Arie_de_filmes)",
+      tags: "fantasia aventura épico tolkien fantasiaépica magia hobbit"
+    },
+    {
+      titulo: "Interstellar",
+      descricao: "Um grupo de astronautas empreende uma missão através de um buraco de minhoca, para encontrar um novo lar para a humanidade, após a Terra se tornar inabitável.",
+      link: "https://pt.wikipedia.org/wiki/wiki/Interstellar_(filme)",
+      tags: "ficção científica espaço viagem no tempo drama ciência futurismo"
+    },
+    {
+      titulo: "O Poderoso Chefão",
+      descricao: "A história da família Corleone, uma poderosa máfia italiana nos Estados Unidos, e a ascensão de Michael Corleone ao poder.Obra essencial para quem adora cinema.",
+      link: "https://pt.wikipedia.org/wiki/O_Poderoso_Chef%C3%A3o",
+      tags: "drama crime máfia família anos 70 francis ford coppola italia"
+    },
+    {
+      titulo: "Coringa",
+      descricao: "A origem trágica do vilão mais famoso da DC Comics. Uma exploração profunda na mente de um homem à beira da loucura.",
+      link: "https://pt.wikipedia.org/wiki/Coringa_(filme_de_2019)",
+      tags: "super-herói psicológico drama dc comics todd phillips gotham vilão"
+    },
+    {
+      titulo: "Vingadores: Ultimato",
+      descricao: "Os Vingadores se reúnem mais uma vez para desfazer o estalo de Thanos e trazer de volta todos que foram transformados em pó.",
+      link: "https://pt.wikipedia.org/wiki/Vingadores:_Ultimato",
+      tags: "super-herói ação aventura marvel vingadores thanos"
+    },
+    {
+      titulo: "O Regresso",
+      descricao: "Um caçador é abandonado para morrer por seu parceiro de trabalho após ser gravemente ferido. Ele deve lutar pela sobrevivência em um ambiente hostil.",
+      link: "https://pt.wikipedia.org/wiki/O_Regresso_(filme_de_2015)",
+      link: "https://pt.wikipedia.org/wiki/O_Regresso_(filme_de_2015)",
+      tags: "sobrevivência aventura drama natureza leonardo dicaprio"
+    }
+  ];
